b23020dd3683654f38ead4e73987c7a419d15ec6
Vildanovi <vildanovi@yandex.ru>
2024-01-08 16:27:15 +0300

    pom

--- src/main/java/ru/practicum/user/UserRepository.java
+++ src/main/java/ru/practicum/user/UserRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByEmailContainingIgnoreCase(String emailSearch);
+
+    List<UserShort> findAllByEmailContainingIgnoreCase(String emailSearch);
 }
